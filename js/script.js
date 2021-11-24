900638169e3f7dff6eaed4d29b7ee87db5480bcd
Sreenath <sreenath4294@gmail.com>
2021-11-24 11:14:39 +0530

    add smooth scroll navigation

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//////////////////////////////////////////////////
+// Set current year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+/////////////////////////////////////////////////
+// Mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+////////////////////////////////////////////////
+// SMooth scroll animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
