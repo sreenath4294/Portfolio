f0dad4555db3e6f32d90496c12678f1d4aa5df5d
Sreenath <sreenath4294@gmail.com>
2021-11-26 09:45:32 +0530

    update menu icon in mobileview

--- js/script.js
+++ js/script.js
@@ -61,6 +61,7 @@ const observer = new IntersectionObserver(function (entries) {
   const navHomeEl = document.querySelector('.link-home');
   const navProductsEl = document.querySelector('.link-products');
   const navAboutEl = document.querySelector('.link-about');
+  const iconMenuEl = document.querySelector('.icon-mobile-nav');
   if (!ent.isIntersecting) {
     headerEl.style.backgroundColor = "#fff";
     headerEl.style.boxShadow = "0 1.2rem 3.2rem rgba(0, 0, 0, 0.3)";
@@ -68,6 +69,7 @@ const observer = new IntersectionObserver(function (entries) {
     navHomeEl.style.color = "#333";
     navProductsEl.style.color = "#333";
     navAboutEl.style.color = "#333";
+    iconMenuEl.style.color = "#333";
   }
   if (ent.isIntersecting) {
     headerEl.style.backgroundColor = "rgba(0, 0, 0, 0.247)";
@@ -76,6 +78,7 @@ const observer = new IntersectionObserver(function (entries) {
     navHomeEl.style.color = "#fff";
     navProductsEl.style.color = "#fff";
     navAboutEl.style.color = "#fff";
+    iconMenuEl.style.color = "#fff";
   }
 }, {
   root: null,
